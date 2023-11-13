5224b010b0f8e93484233dcd55edf50b4aba2cfb
EmilySalcedo12345 <120775640+EmilySalcedo12345@users.noreply.github.com>
2023-11-12 23:53:20 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,46 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const chartContainer = document.getElementById("chart-container");
-    const chartCanvas = document.getElementById("myChart");
-    const currencySelect = document.getElementById("currency");
-    let myChart;  // Variable para almacenar la instancia de Chart
-  
-    // Al cargar la página, muestra la comparación del valor de la moneda por defecto (USD)
-    fetchExchangeRate();
-  
-    // Agregar un evento de cambio al selector de monedas
-    currencySelect.addEventListener('change', fetchExchangeRate);
-  
-    async function fetchExchangeRate() {
-      const baseCurrency = currencySelect.value;
-  
+let myChart; 
 
-      if (myChart) {
-        myChart.destroy();
-      }
-  
-      const response = await fetch(`https://api.frankfurter.app/latest?from=${baseCurrency}`);
-      const data = await response.json();
-      const exchangeRate = data.rates.USD;
-  
-      const chartData = {
-        labels: [baseCurrency, 'USD'],
-        datasets: [{
-          label: `Valor de 1 ${baseCurrency} en USD`,
-          data: [1, exchangeRate],
-          backgroundColor: ['rgba(255, 99, 132, 0.2)', 'rgba(75, 192, 192, 0.2)'],
-          borderColor: ['rgba(255, 99, 132, 1)', 'rgba(75, 192, 192, 1)'],
-          borderWidth: 1
-        }]
-      };
-  
-      displayChart(chartData);
-    }
-  
-    function displayChart(data) {
-  
-      myChart = new Chart(chartCanvas, {
-        type: 'bar',
-        data: data,
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
+function getExchangeRate() {
+  const selectedCurrency = document.getElementById('currency').value;
+  const apiUrl = `https://api.frankfurter.app/latest?from=${selectedCurrency}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const rates = data.rates;
+      const labels = Object.keys(rates);
+      const values = Object.values(rates);
+
+      updateChart(labels, values);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+function updateChart(labels, values) {
+
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  const ctx = document.getElementById('myChart').getContext('2d');
+  myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Valor de la moneda',
+        data: values,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
         }
-      });
+      }
     }
   });
+}
